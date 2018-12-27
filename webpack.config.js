6f17727acc3224d4cbcb8612334121735d7322ae
qianlibin <qianlibin@up360.com>
2018-12-27 17:04:22 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const uglifyjs = require('uglifyjs-webpack-plugin');
 module.exports = {
-	module: {
-		rules: [{
-			test: /\.js$/,
-			loader: 'babel-loader',
-			exclude: /node_modules/
-		}]
-	},
+	// module: {
+	// 	rules: [{
+	// 		test: /\.js$/,
+	// 		loader: 'babel-loader',
+	// 		exclude: /node_modules/
+	// 	}]
+	// },
 	plugins: [
 		new uglifyjs({
 			uglifyOptions: ({
